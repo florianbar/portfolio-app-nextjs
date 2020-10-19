384a5ef41d20dfd2341966a718a9d765a746582a
florianbar <florianbar01@gmail.com>
2020-10-19 09:44:41 +0200

    Created Layout component with styles

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,12 @@
 import Link from 'next/link';
 
+import Layout from '../components/Layout';
+
 const About = () => (
-    <div>
-        <h1>About</h1>
-        <Link href="/">
-            <a>Go to home</a>
-        </Link>
+    <Layout title="About">
         <p>I'm a JavaScript programmer</p>
         <img src="/static/javascript-logo.png" alt="JavaScript" style={{ height: "150px" }} />
-    </div>
+    </Layout>
 );
 
 export default About;
---
+++ pages/hireme.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+import Layout from '../components/Layout';
+
+const HireMe = () => (
+    <Layout title="Hire Me">
+        <p>You can hire me at <a href="mailto:expertcode@gmail.com"></a></p>
+    </Layout>
+);
+
+export default HireMe;
